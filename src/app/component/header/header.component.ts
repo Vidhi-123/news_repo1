824d004ab0de9f5b8ea24f7d645888bd67e4749f
vidhi-123 <vidhipshah10@gmail.com>
2020-06-03 18:54:01 +0530

    new header set

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -15,8 +15,7 @@ export class HeaderComponent implements OnInit {
   onClickDrp(){
 	document.getElementById('drp').classList.add('active');
 	document.getElementById('in').classList.remove('active');
-	  document.getElementById('ch').classList.remove('active');
-	  document.getElementById('con').classList.remove('active');
+	 
 	  document.getElementById('ho').classList.remove('active');
   }
 
@@ -26,33 +25,18 @@ export class HeaderComponent implements OnInit {
   onClickHo(){
 	document.getElementById('ho').classList.add('active');
 	document.getElementById('in').classList.remove('active');
-	  document.getElementById('ch').classList.remove('active');
-	  document.getElementById('con').classList.remove('active');
+	
 	  document.getElementById('drp').classList.remove('active');
   }
   onClickIn()
   {
 	  document.getElementById('in').classList.add('active');
-	  document.getElementById('ch').classList.remove('active');
-	  document.getElementById('con').classList.remove('active');
+	
 	  document.getElementById('ho').classList.remove('active');
 	  document.getElementById('drp').classList.remove('active');
   }
-  onClickCh()
-  {
-	  document.getElementById('ch').classList.add('active');
-	  document.getElementById('in').classList.remove('active');
-	  document.getElementById('con').classList.remove('active');
-	  document.getElementById('ho').classList.remove('active');
-	  document.getElementById('drp').classList.remove('active');
-  }
-  OnClickCont(){
-	document.getElementById('con').classList.add('active');
-	document.getElementById('in').classList.remove('active');
-	document.getElementById('ch').classList.remove('active');
-	document.getElementById('ho').classList.remove('active');
-	document.getElementById('drp').classList.remove('active');
-  }
+  
+  
 
   ngOnInit() {
 	
